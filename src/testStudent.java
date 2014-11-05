e65e71e0daa718a1a685894189cbbc06000f8f02
AK1729 <AK1729@PC-00105764.uni.mdx.ac.uk>
2014-11-05 17:57:48 +0000

    test student

---
+++ src/testStudent.java
@@ -0,0 +1,9 @@
+
+public class testStudent {
+	public static void main(String[] args) {
+		student firstStudent = new student ("John", 19, "Male ", "Biology");
+		firstStudent.displayStudentDetails()
+		;
+	}
+	
+}
